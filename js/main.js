838ee084cd0f5132d53b6ec0c5da876a7089328e
jerodestapa <jerodestapa@gmail.com>
2016-02-11 22:34:58 -0600

    Added parallax image

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // jQuery and parallax.js
 
 $(document).ready(function() {
-    $('.parallax').parallax();
+    $('.parallax').parallax({imageSrc: '/images/profile.jpeg'});
 });
 
 // jQuery and fullPage.js
@@ -15,9 +15,17 @@ $(document).ready(function() {
 
 // Smooth scrolling
 
-$('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 500);
-    return false;
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
