0a30cefed6ca81d491e2db677e3f28951ef82128
jerodestapa <jerodestapa@gmail.com>
2016-02-11 21:27:26 -0600

    Added jquery for fullPage.js and smooth scrolling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
-// jQuery and parallax.js for image effect
-
-var $ = require('jQuery');
+// jQuery and parallax.js
 
 $(document).ready(function() {
     $('.parallax').parallax();
+});
+
+// jQuery and fullPage.js
+
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        autoScrolling: false,
+        fitToSection: false
+    });
+});
+
+// Smooth scrolling
+
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
 });
